e5afca3d572e407a4dea7a8388e35357ba1c81ab
jdev08 <jdevv08@gmail.com>
2024-02-12 17:18:12 +0900

    feat: 기술 문서 검토 페이지 와이어프레임

---
+++ src/components/DefaultLayout.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+export default function DefaultLayout({ children }: Props) {
+  return <Wrapper>{children}</Wrapper>;
+}
+
+const Wrapper = styled.div`
+  padding: 0 60px;
+`;
