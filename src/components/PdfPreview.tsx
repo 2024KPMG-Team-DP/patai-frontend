e5b2ba76ee9c7c8f5d4dce0faf26ed26cf8d925c
jdev08 <jdevv08@gmail.com>
2024-02-14 21:04:53 +0900

    feat: pdf 프리뷰

--- src/components/PdfPreview.tsx
+++ src/components/PdfPreview.tsx
@@ -1,35 +1,46 @@
 import styled from "styled-components";
+import { pdfjs } from "react-pdf";
+import { Document, Page } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+import { useState } from "react";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
+).toString();
 
 interface Props {
   url?: string | null;
 }
 
 export default function PdfPreview({ url }: Props) {
+  const [numPages, setNumPages] = useState<number | null>(null);
+
+  const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
+    setNumPages(numPages);
+  };
+
   return (
     <Wrapper>
-      <PreviewBox>
-        <Content>{url || ""}</Content>
-      </PreviewBox>
+      {url && (
+        <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
+          {Array.from(new Array(numPages), (el, index) => (
+            <Page
+              key={`page_${index + 1}`}
+              pageNumber={index + 1}
+              height={600}
+            />
+          ))}
+        </Document>
+      )}
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
   width: 100%;
-`;
-
-const PreviewBox = styled.div`
-  width: 100%;
-  border-radius: 10px;
-  border: 2px solid rgba(0, 0, 0, 0.5);
-  background: #fff;
-`;
-
-const Content = styled.div`
-  margin: 10px;
-  overflow-wrap: break-word;
-  word-wrap: break-word;
-  height: 600px;
-  overflow-x: hidden;
-  overflow-y: scroll;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
 `;
